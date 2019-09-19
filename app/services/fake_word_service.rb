706b7753cc5811ebbac53ec250d85b7a196a9d07
Thomas <feral.thomas@gmail.com>
2019-09-19 14:10:55 +0200

    fakewordservice

---
+++ app/services/fake_word_service.rb
@@ -0,0 +1,12 @@
+class FakeWordService
+  def initialize(origin)
+    File.open(origin, 'r') do |file|
+      file.each do |line|
+        content, counter = line.split
+        unless FakeWord.all.pluck(:content).include? content
+          FakeWord.create(content: content, counter: counter.to_i)
+        end
+      end
+    end
+  end
+end
--- app/services/scrapper_service.rb
+++ app/services/scrapper_service.rb
@@ -1,17 +1,6 @@
 class ScrapperService
-  def initialize(origin)
-    File.open(origin, 'r') do |file|
-      file.each do |line|
-        content, counter = line.split
-        unless FakeWord.all.pluck(:content).include? content
-          FakeWord.create(content: content, counter: counter.to_i)
-        end
-      end
-    end
-  end
-
-  def process
-    FakeWord.all.each do |fake_word|
+  def initialize(fake_words)
+    fake_words.each do |fake_word|
       DeclinaisonScrapperService.new(fake_word).process
       ConjugateScrapperService.new(fake_word).proces
     end
